7216977a2a9d2af5203f08cea4957ea20d7db267
Inamov Asilbek <playerallgames007@gmail.cpm>
2024-06-08 12:57:56 +0500

    Fix03

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav_header from "./components/nav_header/nav_header.jsx"
 import Menu_part from "./components/menu_part/menu_part.jsx"
+import Btn_part from "./components/btn_part/btn_part.jsx"
 import "./index.css"
 
 export default function App() {
   return (
     <div className="all_React">
       <Nav_header />
+      <Btn_part />
       <Menu_part />
     </div>
   )
---
+++ src/components/btn_part/btn_part.jsx
@@ -0,0 +1,65 @@
+import "./btn_part.css"
+
+import coin_01 from "../../assets/btn_part_img/coin_01.png"
+import coin_02 from "../../assets/btn_part_img/coin_02.png"
+import energy_01 from "../../assets/btn_part_img/energy.png"
+import boost_01 from "../../assets/btn_part_img/boost.png"
+import i_znak from "../../assets/btn_part_img/i_znak.png"
+import hams_coin_01 from "../../assets/btn_part_img/hamster_kombat_click.png"
+
+
+export default function btn_part() {
+    return (
+        <>
+            <div className="all_hams">
+                <div className="hams_manu">
+                    <div className="hams_menu_part_01">
+                        <p className="hams_menu_P">Прибыль за тап</p>
+                        <div className="img_menu_part_coin">
+                            <img src={coin_01} alt="" />
+                            <p className="sum_cin_part">+15</p>
+                        </div>
+                    </div>
+                    <div className="hams_menu_part_01">
+                        <p className="hams_menu_P">Монет для апа</p>
+                        <div className="img_menu_part_coin">
+                            <p className="sum_cin_part">35K</p>
+                        </div>
+                    </div>
+                    <div className="hams_menu_part_01">
+                        <p className="hams_menu_P">Прибыль в час</p>
+                        <div className="img_menu_part_coin">
+                            <img src={coin_01} alt="" />
+                            <p className="sum_cin_part">+15</p>
+                            <img src={i_znak} alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="hams_sum">
+                    <img src={coin_02} alt="" />
+                    <h1 className="coin_sum">3 000</h1>
+                </div>
+                <div className="hams_line">
+                    <div className="up_hams_line">
+                        <p className="level_p_01">Diamond</p>
+                        <p className="level_P_02">Level 7/10</p>
+                    </div>
+                    <div className="div_line_01"></div>
+                </div>
+                <div className="hams_clickk">
+                    <img src={hams_coin_01} alt="" />
+                </div>
+                <div className="hams_energy">
+                    <div className="energy">
+                        <img src={energy_01} alt="" />
+                        <p className="energy_P_01">10000 / 15000</p>
+                    </div>
+                    <div className="boost">
+                        <img src={boost_01} alt="" />
+                        <p className="boost_P_01">Boost</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
